e26b0ac3e3edb6fa739519b53dcf66e0ee92d385
yunwhanlee <yunwhanlee@gmail.com>
2024-10-20 19:08:31 +0900

    Setting Localization

--- Assets/Scripts/Home/SettingManager.cs
+++ Assets/Scripts/Home/SettingManager.cs
@@ -14,13 +14,37 @@ public class SettingManager : MonoBehaviour
     public Slider bgmSlider;
     public Slider sfxSlider;
 
-    void Start()
+    public GameObject languagePanel;
+    public Sprite[] languageIconSprs;
+    public Image languageSelectBtnImg;
+
+    IEnumerator Start()
     {
+        // 데이터가 먼저 로드될때까지 대기
+        yield return new WaitUntil(() => DM._.DB != null);
+
         bgmSlider.value = DM._.DB.bgmVolume;
         sfxSlider.value = DM._.DB.sfxVolume;
     }
 
 #region EVENT
+    public void OnClickSelectLanguage()
+    {
+        languagePanel.SetActive(true);
+    }
+
+    /// <summary>
+    /// 언어변경 버튼 클릭
+    /// </summary>
+    /// <param name="lauguageIdx">언어종류 인덱스(EN:0, JP:1, KR:2)</param>
+    public void OnClickChangeLanguage(int lauguageIdx)
+    {
+        SoundManager._.PlaySfx(SoundManager.SFX.Tap2SFX);
+        LocaleManager._.ChangeLocale(lauguageIdx);
+        languagePanel.SetActive(false);
+        languageSelectBtnImg.sprite = languageIconSprs[lauguageIdx];
+    }
+
     public void OnClickResetBtn()
     {
         GM._.ui.ShowConfirmPopUp("정말로 데이터를 리셋하겠습니까?");
