88254bff682baeed426c18e8391e7fd8c01df601
yunwhanlee <yunwhanlee@gmail.com>
2024-12-01 19:35:40 +0900

    Debug로그 최적화

---
+++ Assets/Scripts/public/Class/Debug.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Diagnostics;
+
+public static class Debug {
+    private static bool IsDebugging {
+        get {
+            #if UNITY_EDITOR
+                return true;
+            #elif UNITY_ANDROID
+                return DM._?.IsDebugMode ?? false;
+            #else
+                return false;
+            #endif
+        }
+    }
+
+    // 전처리 기능 Attribute
+    [Conditional("UNITY_EDITOR")]
+    public static void Break() =>
+        UnityEngine.Debug.Break();
+
+    [Conditional("UNITY_EDITOR")]
+    public static void ClearDeveloperConsole() =>
+        UnityEngine.Debug.ClearDeveloperConsole();
+
+    #if UNITY_EDITOR 
+    public static void Log(object msg) {
+        UnityEngine.Debug.Log(msg);
+    }
+    #elif UNITY_ANDROID
+    public static void Log(object msg) {
+        if(IsDebugging)
+            UnityEngine.Debug.Log(msg);
+    }
+    #endif
+
+    #if UNITY_EDITOR
+    public static void LogError(object msg) {
+        UnityEngine.Debug.LogError(msg);
+    }
+    #elif UNITY_ANDROID
+    public static void LogError(object msg) {
+        if(IsDebugging)
+            UnityEngine.Debug.LogError(msg);
+    }
+    #endif
+
+    [Conditional("UNITY_EDITOR")] 
+    public static void LogWarning(object msg) =>
+        UnityEngine.Debug.LogWarning(msg);
+
+    [Conditional("UNITY_EDITOR")]
+    public static void DrawRay(Vector3 start, Vector3 dir) =>
+        UnityEngine.Debug.DrawRay(start, dir);
+
+    [Conditional("UNITY_EDITOR")]
+    public static void DrawRay(Vector3 start, Vector3 dir, Color color) =>
+        UnityEngine.Debug.DrawRay(start, dir, color);
+
+    [Conditional("UNITY_EDITOR")]
+    public static void DrawRay(Vector3 start, Vector3 dir, Color color, float duration) =>
+        UnityEngine.Debug.DrawRay(start, dir, color, duration);
+
+    [Conditional("UNITY_EDITOR")]
+    public static void DrawRay(Vector3 start, Vector3 dir, Color color, float duration, bool depthTest) =>
+        UnityEngine.Debug.DrawRay(start, dir, color, duration, depthTest);
+}
